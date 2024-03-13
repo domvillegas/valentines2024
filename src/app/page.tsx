2351be21fb40bfe5b6163870517443120c15ba09
domvillegas <domvillgithub@gmail.com>
2024-03-13 00:36:05 -0700

    Good will

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,16 @@ export default function Home() {
   return (
     <div className="wrap">
       <div className="poem">
+        <a className="line" href="https://gemini.google.com/app">https://gemini.google.com/app</a>
         <div className="line">10000011110011001101011</div>
         <div className="line">Gemini: "I'm the moon."</div>
         <div className="line">Read response.</div>
         <div className="line">Reply: "Yes, but please call me queen."</div>
-        <div className="line">Play 	&#9829;</div>
+        <div className="line">Play &#9829;</div>
         <div className="line">10010111010100001011111</div>
         <div className="line"></div>
+        <a className="line" href="https://gemini.google.com/app">https://gemini.google.com/app</a>
+
       </div>
     </div>
   );
