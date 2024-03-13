28323a57e8b10446159dc4e7271481a9b80933f5
domvillegas <domvillgithub@gmail.com>
2024-03-13 00:24:20 -0700

    10001101011110101010

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div className="wrap">
       <div className="poem">
-        <div className="line1">10000011110011001101011</div>
+        <div className="line">10000011110011001101011</div>
+        <div className="line">Gemini: "I'm the moon."</div>
+        <div className="line">Read response.</div>
+        <div className="line">Reply: "Yes, but please call me queen."</div>
+        <div className="line">Play 	&#9829;</div>
+        <div className="line">10010111010100001011111</div>
+        <div className="line"></div>
       </div>
     </div>
   );
