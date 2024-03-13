b1197406a73c8d2683eadb8deecedf0bf0750ed2
domvillegas <domvillgithub@gmail.com>
2024-03-12 23:52:07 -0700

    Understood

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="wrap">
       <div className="poem">
-        <div className="line1">&#9829;</div>
+        <div className="line1">10000011110011001101011</div>
       </div>
     </div>
   );
