686bd6f8ec9b974c72c4344798ba4aa8abe55128
domvillegas <domvillgithub@gmail.com>
2024-03-13 00:37:38 -0700

    Target blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div className="wrap">
       <div className="poem">
-        <a className="line" href="https://gemini.google.com/app">https://gemini.google.com/app</a>
+        <a
+          className="line"
+          target="_blank"
+          href="https://gemini.google.com/app"
+        >
+          https://gemini.google.com/app
+        </a>
         <div className="line">10000011110011001101011</div>
         <div className="line">Gemini: "I'm the moon."</div>
         <div className="line">Read response.</div>
@@ -13,8 +19,13 @@ export default function Home() {
         <div className="line">Play &#9829;</div>
         <div className="line">10010111010100001011111</div>
         <div className="line"></div>
-        <a className="line" href="https://gemini.google.com/app">https://gemini.google.com/app</a>
-
+        <a
+          className="line"
+          target="_blank"
+          href="https://gemini.google.com/app"
+        >
+          https://gemini.google.com/app
+        </a>
       </div>
     </div>
   );
